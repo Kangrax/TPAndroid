2ee0ee38779229d4090511f9a5bca9a906e8446a
Eiyeron FULMICENDII <eiyeron@gmail.com>
2014-03-21 13:58:14 +0100

    Started COntact add popup

--- gen/com/Eiyeron/tpandroid/R.java
+++ gen/com/Eiyeron/tpandroid/R.java
@@ -35,24 +35,33 @@ public final class R {
         public static final int blue_button=0x7f020000;
         public static final int green_button=0x7f020001;
         public static final int ic_contact_item_blank_icon=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int red_button=0x7f020004;
-        public static final int yellow_button=0x7f020005;
+        public static final int ic_contact_take_photo=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int red_button=0x7f020005;
+        public static final int yellow_button=0x7f020006;
     }
     public static final class id {
-        public static final int RelativeLayout1=0x7f090004;
-        public static final int action_settings=0x7f09000c;
-        public static final int blue_button=0x7f090008;
-        public static final int contact_detail_first_name=0x7f090001;
-        public static final int contact_detail_last_name=0x7f090002;
-        public static final int contact_item_icon=0x7f090005;
-        public static final int contact_item_name=0x7f090006;
-        public static final int contact_item_number=0x7f090007;
-        public static final int green_button=0x7f09000a;
-        public static final int imageView1=0x7f090000;
-        public static final int list=0x7f090003;
-        public static final int red_button=0x7f090009;
-        public static final int yellow_button=0x7f09000b;
+        public static final int ImageButton1=0x7f090008;
+        public static final int LinearLayout1=0x7f090006;
+        public static final int LinearLayout2=0x7f090002;
+        public static final int SaloperieDImage=0x7f090000;
+        public static final int action_settings=0x7f090014;
+        public static final int blue_button=0x7f090010;
+        public static final int contact_add_first_name_field=0x7f090003;
+        public static final int contact_add_last_name_field=0x7f090004;
+        public static final int contact_add_phone_number_field=0x7f090005;
+        public static final int contact_add_photo=0x7f090007;
+        public static final int contact_detail_first_name=0x7f09000a;
+        public static final int contact_detail_last_name=0x7f09000b;
+        public static final int contact_item_icon=0x7f09000d;
+        public static final int contact_item_name=0x7f09000e;
+        public static final int contact_item_number=0x7f09000f;
+        public static final int green_button=0x7f090012;
+        public static final int imageView1=0x7f090009;
+        public static final int linearLayout1=0x7f090001;
+        public static final int list=0x7f09000c;
+        public static final int red_button=0x7f090011;
+        public static final int yellow_button=0x7f090013;
     }
     public static final class layout {
         public static final int activity_ajout_contact=0x7f030000;
@@ -71,6 +80,7 @@ public final class R {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
         public static final int blue_button_text=0x7f060005;
+        public static final int contact_add_phone_number=0x7f06000e;
         public static final int contact_detail_first_name=0x7f06000c;
         public static final int contact_detail_last_name=0x7f06000d;
         public static final int contact_item_name_placeholder=0x7f060009;
--- src/com/Eiyeron/tpandroid/MainActivity.java
+++ src/com/Eiyeron/tpandroid/MainActivity.java
@@ -1,17 +1,51 @@
 package com.Eiyeron.tpandroid;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
+	private final int ADD_CONTACT_PHOTO = 1;
+	private final int ADD_CONTACT = 2;
+	private final String TMP_FILE = Environment.getExternalStorageDirectory()
+			.getPath();
+	private View dialogView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+
+	// Problème avec les photos.
+
+	public void ajoutContact(View v) {
+		// Inflation du layout du Dialog, puis affichage de
+		dialogView = View.inflate(this, R.layout.activity_ajout_contact, null);
+		AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+		dialog.setView(dialogView);
+		dialog.show();
+	}
+
+	public void addContactPhoto(View v) {
+		// On appelle
+		Intent i = new Intent(Intent.ACTION_PICK,
+				android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+		startActivityForResult(i, ADD_CONTACT_PHOTO);
+	}
+
+	public void accederRepertoire(View v) {
+		Intent intentRepertoire = new Intent(this, ListContact.class);
+		startActivity(intentRepertoire);
 	}
 
 	@Override
@@ -20,16 +54,30 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
-	
-	public void ajoutContact(View v) {
-		Intent intentAjout = new Intent(this, AjoutContact.class);
-		startActivity(intentAjout);
-	}
 
-	public void accederRepertoire(View v) {
-		Intent intentRepertoire = new Intent(this, ListContact.class);
-		startActivity(intentRepertoire);
-	}
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (requestCode == ADD_CONTACT_PHOTO) {
+			// Changement de l'image du contact à ajouter
+			if (resultCode == RESULT_CANCELED)
+				return;
 
+			Uri uri = data.getData();
+			String[] filePathColumn = { android.provider.MediaStore.Images.Media.DATA };
+			Cursor cursor = getContentResolver().query(uri, filePathColumn,
+					null, null, null);
+			cursor.moveToFirst();
+
+			int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+			String picturePath = cursor.getString(columnIndex);
+			cursor.close();
+			Log.d("Meh", picturePath);
+			ImageButton iButton = (ImageButton) dialogView
+					.findViewById(R.id.contact_add_photo);
+
+			iButton.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+			Log.e("Meh", "NPE");
+		}
+
+	}
 }
